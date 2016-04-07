76e8dc39f3eb31c1d597638420d09a2f01d12dd2
spiderOO7 <shingwl@yahoo.com>
2016-04-08 00:15:19 +0530

    sign up exersize

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
   end
 
   test "valid signup information" do
@@ -22,6 +25,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
   
 end
