9b8e547e228707b6151563d991faefcd88df283b
spiderOO7 <shingwl@yahoo.com>
2016-04-07 23:38:46 +0530

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,43 @@
 class UsersController < ApplicationController
-  def new
+
+  def index
   end
+
+  def new
+  	@user = User.new
+  end
+
+  
+  def show
+  	@user = User.find(params[:id])
+
+  	#-- Start a debugging session in server(byebugs)
+  	# debugger
+  end
+
+  def create
+  	 @user = User.new(user_params)
+    if @user.save
+    	flash[:success] = "Welcome to the Sample App!"
+    	redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
