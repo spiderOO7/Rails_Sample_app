230ec3dcb113f011c3dfcb1191cca0aeb3985e89
spiderOO7 <shingwl@yahoo.com>
2016-04-09 21:43:57 +0530

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar" )
+    @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   #####################
@@ -19,6 +20,18 @@ class UsersControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
+
+  # test "should redirect edit when not logged in" do
+  #   get :edit, id: @user
+  #   assert_not flash.empty?
+  #   assert_redirected_to login_url
+  # end
+
+  # test "should redirect update when not logged in" do
+  #   patch :update, id: @user, user: { name: @user.name, email: @user.email }
+  #   assert_not flash.empty?
+  #   assert_redirected_to login_url
+  # end
 
   test "should be valid" do
     assert @user.valid?
@@ -77,4 +90,37 @@ class UsersControllerTest < ActionController::TestCase
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
 end
